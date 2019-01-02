79ef0017cb34dda7f829ff7c0028bc1fbc508e0c
karuru6225 <e2hjhi@gmail.com>
2019-01-03 00:59:22 +0900

    同じファイルなのに重複して初期化されてた不具合修正。

--- src/js/components/movie/index.jsx
+++ src/js/components/movie/index.jsx
@@ -14,25 +14,29 @@ class Movie extends React.Component {
   constructor(props) {
     super(props);
     this.player = null;
+    this.id = null;
   }
 
   initPlayer() {
     const {
       current
     } = this.props;
-    this.player = videojs(`video-${current.id}`, {
-      controls: true,
-      html5: {
-        hls: {
-          withCredentials: true
+    if (this.id !== current.id) {
+      this.player = videojs(`video-${current.id}`, {
+        controls: true,
+        html5: {
+          hls: {
+            withCredentials: true
+          }
         }
-      }
-    });
-    const src = `${process.env.API_ENTRY}/file/${current.id}/direct`;
-    this.player.src({
-      src,
-      type: 'application/x-mpegURL'
-    });
+      });
+      const src = `${process.env.API_ENTRY}/file/${current.id}?open`;
+      this.player.src({
+        src,
+        type: 'application/x-mpegURL'
+      });
+      this.id = current.id;
+    }
   }
 
   componentDidMount() {
